be1c2bf80b67db92a583f71a3a91167df9a60ae3
souravmali <souravmali6516@gmail.com>
2018-07-28 16:25:32 +0530

    sr add

---
+++ cal.py
@@ -0,0 +1,15 @@
+from calci import Calculator
+from calci import ScientificCalculator, print_something
+
+obj = ScientificCalculator("some")
+
+print(obj.power(6))
+
+
+objcal=Calculator("addition")
+resultadd=objcal.add(5,6)
+print(resultadd)
+
+print
+
+print_something("sourav")
